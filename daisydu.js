c23c81c7bdf0796f46ca961e85bb27e33dae47db
alexisjanvier <contact@alexisjanvier.net>
2014-09-27 15:39:33 +0200

    Use d3 treemap to dosplay folders occupation disk in drawille

--- daisydu.js
+++ daisydu.js
@@ -2,9 +2,11 @@
 
 'use strict';
 
-// var d3 = require('./lib/d3Canvas');
-// var canvas = require('drawille-canvas');
-// var context = new canvas(160, 160);
+var d3 = require('./lib/d3Canvas');
+var canvas = require('drawille-canvas');
+var width = 160,
+    height = 160;
+var context = new canvas(width, height);
 // var jsdom = require('jsdom');
 // var  htmlStub = '<html><body><div id="canvas"></div></body></html>';
 
@@ -31,7 +33,60 @@ var startPath = './';
 
 var folder = new FolderAnalyser(startPath);
 folder.analyse().then(function(){
-    console.log('This folder (' + folder.absolutePath + ') size is ' + (folder.totalSize / 1024 / 1024).toFixed(2) + ' Mb');
+    //console.log('This folder (' + folder.absolutePath + ') size is ' + (folder.totalSize / 1024 / 1024).toFixed(2) + ' Mb');
+    var treemapData = folder.getTreemapDatas();
+    //console.log(treemapData);
+
+
+    var treemap = d3.layout.treemap()
+        .size([width, height])
+        .sort(function(a, b) {
+            return a.value - b.value;
+        }).mode('squarify');
+
+    var treemap = d3.layout.treemap()
+        //.children(function(d) { return d.text})
+        //.children(function (d) {return d.array})
+        .children(function (d) {return d.children})
+        .size([width,height])
+        .value(function (d) {return d.size})
+        // .sort(function(a, b) {
+        //     return a.value - b.value;
+        // })
+        .mode('squarify')
+        .nodes(treemapData);
+
+    //console.log(treemap);
+
+    //     var testit = treemapData.map(function(r) {
+    //         return { value: r };
+    //     });
+    //     console.log(testit);
+
+    // var data = {
+    //     value:0,
+    //     children: treemapData
+    // };
+    
+
+    //var map = treemap.nodes(treemapData);
+    
+    //context.fillRect(0, 0, width, height);
+    function position(d, i) {
+      //if (d.children) return;
+      //context.fillStyle = (d.children) ? '#fff' : color(i);
+      context.fillRect(~~d.x, ~~d.y,
+          ~~Math.max(0, d.dx),
+          ~~Math.max(0, d.dy));
+      context.clearRect(~~d.x + 1, ~~d.y + 1,
+          ~~Math.max(0, d.dx) - 3,
+          ~~Math.max(0, d.dy) -3);
+    }
+     
+     treemap.forEach(position);
+     console.log(context._canvas.frame());
+
+
 },function (error) {
     console.log(error);
 });

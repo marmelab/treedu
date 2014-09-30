2c7e3377e29a9ee85c9e4139f004dc1154f2ff8b
alexisjanvier <contact@alexisjanvier.net>
2014-09-30 11:02:40 +0200

    Resizing console, select subfolder and waiting

--- app.js
+++ app.js
@@ -8,17 +8,19 @@
 'use strict';
 
 var App = function() {
+    
     var d3 = require('./lib/d3Canvas'),
-    fs = require('q-io/fs'),
-    Canvas = require('drawille-canvas'),
-    FolderAnalyzer = require('./lib/folderAnalyzer'),
-    blessed = require('blessed'),
-    cli = require('commander'),
-    os = require('os'),
-    glob = require('glob'),
-    VERSION = require('./package.json').version,
-    themes = '';
+        fs = require('q-io/fs'),
+        Canvas = require('drawille-canvas'),
+        FolderAnalyzer = require('./lib/folderAnalyzer'),
+        blessed = require('blessed'),
+        cli = require('commander'),
+        os = require('os'),
+        glob = require('glob'),
+        q= require('q'),
+        VERSION = require('./package.json').version;
 
+    var themes;
     var files = glob.sync(__dirname + '/themes/*.json');
     for (var i = 0; i < files.length; i++) {
         var themeName = files[i].replace(__dirname + '/themes/', '').replace('.json', '');
@@ -27,17 +29,19 @@ var App = function() {
     themes = themes.slice(0, -1);
 
     // Set up the commander instance and add the required options
-    cli
-    .option('-p, --path  [absolutePath]', 'path to display', './')
-    .option('-t, --theme  [name]', 'set the vtop theme [' + themes + ']', 'monokai')
-    .version(VERSION)
-    .parse(process.argv);
-
+    cli.option('-p, --path  [path to folder]', 'folder path to display', './')
+        .option('-t, --theme  [name]', 'set the treedu theme [' + themes + ']', 'marmelab')
+        .version(VERSION)
+        .parse(process.argv);
 
     var screen,
          loadedTheme,
          currentPath,
-         activeFolder;
+         activeFolder = '',
+         graph,
+         foldersAnalyse,
+         folderList,
+         waiter;
 
 
     var size = {
@@ -51,13 +55,10 @@ var App = function() {
             }
         };
 
-
     var drawHeader = function() {
         var headerText, headerTextNoTags;
         headerText = ' {bold}treedu{/bold}{white-fg} for ' + os.hostname() + ' ';
         headerTextNoTags = ' treedu for ' + os.hostname() + ' ';
-
-
         var header = blessed.text({
             top: 'top',
             left: 'left',
@@ -71,16 +72,15 @@ var App = function() {
     };
 
     var drawFooter = function() {
-
         var commands = {
-            'q': 'Quit'
+            'q': 'Quit',
+            'up,down' : 'select subdirectory'
         };
         var text = '';
         for (var c in commands) {
             var command = commands[c];
             text += '  {white-bg}{black-fg}' + c + '{/black-fg}{/white-bg} ' + command;
         }
-
         var footerRight = blessed.text({
             bottom: '0',
             left: '0%',
@@ -93,8 +93,7 @@ var App = function() {
         screen.append(footerRight);
     };
 
-     var drawChart = function(treemapData, currentCanvas) {
-
+    var drawChart = function(treemapData, currentCanvas) {
         var treemap = d3.layout.treemap()
         .children(function (d) {return d.children;})
         .size([size.pixel.width,size.pixel.height])
@@ -111,12 +110,11 @@ var App = function() {
                   Math.max(0, d.dx) - 3,
                   Math.max(0, d.dy) -3);
           }
-      }
+        }
+        treemap.forEach(position);
 
-      treemap.forEach(position);
-
-      return currentCanvas._canvas.frame();
-  };
+        return currentCanvas._canvas.frame();
+    };
 
     var getFolderListBySize = function (folderList) {
         folderList.sort(function (a, b) {
@@ -126,10 +124,58 @@ var App = function() {
         });
         var listToDisplay = [];
         folderList.forEach(function(folder){
-            listToDisplay.push( folder.path.replace(currentPath + '/', ' * ') + ' (' + (folder.size / 1024 / 1024).toFixed(2) + ' Mb)' );
+            if (activeFolder === '') {
+                activeFolder = folder.path;
+            }
+            listToDisplay.push( folder.path.replace(currentPath , '') + ' (' + (folder.size / 1024 / 1024).toFixed(2) + ' Mb)' );
         });
 
         return listToDisplay;
+    };
+
+    var waiting = function() {
+            var n = 10;
+            var a = 20;
+            var w = 40;
+            var c = new Canvas(w*2, w);
+            var t = 2;
+            var pi = Math.PI;
+            var pi2 = pi/2;
+            var sin = Math.sin;
+            var cos = Math.cos;
+
+            var flush = function() {
+                var waitingData = c._canvas.frame();
+                graph.setContent('{center}'+ waitingData + '{/center}');
+                screen.render();
+            };
+
+            function draw() {
+                var now = Date.now()/1000;
+                c.clearRect(0, 0, w*2, w*2);
+                c.save();
+                c.translate(w, w);
+                for(var i = 1; i < n; i++) {
+                    var r = i*(w/n);
+                    c.beginPath();
+                    c.moveTo(-r, 0);
+                    var tt = now*pi/t;
+                    var p = (sin(tt-pi*(cos(pi*i/n)+1)/2)+1)*pi2;
+                    for(var j = 0; j < a; j++) {
+                        var ca = pi*j/(a-i);
+                        if(p > ca) {
+                            c.lineTo(-cos(ca)*r, -sin(ca)*r);
+                        } else {
+                            c.lineTo(-cos(p)*r, -sin(p)*r);
+                        }
+                    }
+                    c.stroke();
+                }
+                c.restore();
+                flush();
+            }
+            graph.setLabel(' Waiting... ');
+            waiter = setInterval(draw, 1000/20);
     };
 
     return {
@@ -153,60 +199,91 @@ var App = function() {
                 console.log('The theme \'' + theme + '\' does not exist.');
                 process.exit(1);
             }
-            // Create a screen object.
+
             screen = blessed.screen();
 
-            drawHeader();
-            drawFooter();
+            var drawBoxes = function() {
+                drawHeader();
+                drawFooter();
 
-            var graph = blessed.box({
-                top: 2,
-                left: 'left',
-                width: '70%',
-                height: '90%',
-                content: '',
-                fg: loadedTheme.chart.fg,
-                tags: true,
-                border: loadedTheme.chart.border
+                graph = blessed.box({
+                    top: 2,
+                    left: 'left',
+                    width: '70%',
+                    height: '90%',
+                    content: '',
+                    fg: loadedTheme.chart.fg,
+                    tags: true,
+                    border: loadedTheme.chart.border
+                });
+                screen.append(graph);
+
+                folderList = blessed.list({
+                    top: 2,
+                    left: '70%',
+                    width: screen.width - graph.width,
+                    height: graph.height,
+                    scrollable: true,
+                    keys: true,
+                    mouse: true,
+                    vi: true,
+                    fg: loadedTheme.table.fg,
+                    tags: true,
+                    border: loadedTheme.table.border,
+                    selectedBg: '#EB8118',
+                    selectedBold: true
+                });
+                folderList.setLabel(' Folders list ');
+
+                folderList.on('select', function(data){
+                          var itemContent = data.content.split(' ');
+                          activeFolder = currentPath + itemContent[0];
+                          setfetchFoldersDatas();
+                });
+
+                screen.append(folderList);
+                screen.render();
+            };
+
+            screen.key(['escape', 'q', 'C-c'], function() {
+                return process.exit(0);
             });
-            graph.setLabel(' Current path: ' + currentPath + ' ');
-            screen.append(graph);
 
-            var folderList = blessed.list({
-                top: 2,
-                left: '70%',
-                width: screen.width - graph.width,
-                height: graph.height,
-                keys: true,
-                mouse: true,
-                fg: loadedTheme.table.fg,
-                tags: true,
-                border: loadedTheme.table.border
-            });
-            folderList.setLabel(' Folders list ');
-            screen.append(folderList);
+            drawBoxes();
+            waiting();
+            
+            var fetchFolders = function () {
+                var deferred = q.defer();
+                foldersAnalyse = new FolderAnalyzer(currentPath);
+                foldersAnalyse.analyse().then(function(){
+                    clearInterval(waiter);
+                    graph.setLabel(' Current path: ' + currentPath + ' ');
+                    deferred.resolve(true);
+                },function (error) {
+                    deferred.reject(error); 
+                });
+                return deferred.promise;
+            };
 
-            screen.key(['escape', 'q', 'C-c'], function(ch, key) {
-              return process.exit(0);
-            });
-
-            // Render an empty screen.
-            screen.render();
-
-            // TODO launch something while folder size is compute
-            var folder = new FolderAnalyzer(currentPath);
-            folder.analyse().then(function(){
-                folderList.setItems(getFolderListBySize(folder.folders));
+            var setfetchFoldersDatas = function() {
                 size.pixel.width = (graph.width - 3) * 2;
                 size.pixel.height = (graph.height - 2) * 4;
+
+                folderList.setItems(getFolderListBySize(foldersAnalyse.folders));
+                folderList.width = screen.width - graph.width;
+                folderList.height = graph.height;
+
                 var currentCanvas = new Canvas(size.pixel.width, size.pixel.height);
-                //graph.setContent(drawChart(folder.getTreemapDatas(), currentCanvas));
+                graph.setContent(drawChart(foldersAnalyse.getTreemapDatas(), currentCanvas));
                 folderList.focus();
+
                 screen.render();
-            },function (error) {
-                graph.setContent(error);
-                screen.render();
-            });
+            };
+
+            fetchFolders().then(setfetchFoldersDatas);
+
+            screen.on('resize', setfetchFoldersDatas);
+
         }
     };
 }();
